7d91b3be2744701c6dfd4c68f8218eac5ba1f581
Guillaume Bouton ESME 3S <guillaume.bouton@esme.fr>
2025-02-01 22:23:31 +0100

    Ajout des modifications pour la branche Guillaume (CORRECTION DUN BUG AFFECTANT LA MAP ET BOUTON DE REINITIALISATION MARQUEURS DES LIEUX LIKES FAVORIS STATIQUES)

--- lib/screens/compass_page.dart
+++ lib/screens/compass_page.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_compass/flutter_compass.dart';
+import 'dart:math' as math;
 
-class CompassPage extends StatelessWidget {
+class CompassPage extends StatefulWidget {
+  @override
+  _CompassPageState createState() => _CompassPageState();
+}
+
+class _CompassPageState extends State<CompassPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,9 +16,30 @@ class CompassPage extends StatelessWidget {
         backgroundColor: Theme.of(context).primaryColor,
       ),
       body: Center(
-        child: Text('Compass functionality will be implemented here'),
+        child: StreamBuilder<CompassEvent>(
+          stream: FlutterCompass.events,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text('Error reading heading: ${snapshot.error}');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            }
+
+            double? direction = snapshot.data?.heading;
+
+            if (direction == null) {
+              return Text("Device does not have sensors !");
+            }
+
+            return Transform.rotate(
+              angle: (direction * (math.pi / 180) * -1),
+              child: Image.asset('lib/assets/images/compass.png'),
+            );
+          },
+        ),
       ),
     );
   }
 }
-
