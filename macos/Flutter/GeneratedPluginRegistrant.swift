4cd15ccfba98f84f92f7400001a9764ac855962c
Guillaume83-ESME <guillaume.bouton@esme.fr>
2025-02-02 22:21:37 +0100

    Mise à jour app : AJOUT D'INSTRUCTIONS DE NAVIGATION DE NOUVEAUX PANELS SUR LA MAP ANTHONY ON TE VOIT)

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import app_settings
 import device_info_plus
 import geolocator_apple
 import path_provider_foundation
@@ -13,6 +14,7 @@ import shared_preferences_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppSettingsPlugin.register(with: registry.registrar(forPlugin: "AppSettingsPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
